99b0e915a649a4007462ad08ef7e0971cb5efa31
zhpng <zhaopengvv@163.com>
2022-10-02 21:49:26 +0800

    add stl_construct.h stl_type_traits.h

---
+++ stl_construct.h
@@ -0,0 +1,61 @@
+#ifndef MYSTL_STLCONSTRUCT_H_
+#define MYSTL_STLCONSTRUCT_H_
+#include <new>
+#include "stl_type_traits.h"
+
+// 内存配置后的对象构造和内存释放前的对象析构
+namespace mystl {
+
+template <class T1, class T2>
+inline void construct(T1* p, const T2& value)
+{
+    new (p) T1(value); // placement new; 调用T1:T1(value)，在指定空间生构造对象
+}
+
+// destroy 第二版本，接受一个指针
+template <class T>
+inline void destroy(T* pointer)
+{
+    pointer->~T(); // 调用dtor ~T();
+}
+
+// destroy 第二版本，接受两个迭代器。此函数设法找出元素的数值型别
+// 进而利用__type_traits<>求取最适当的措施
+template <class ForwardIterator>
+inline void destroy(ForwardIterator first, ForwardIterator last)
+{
+    __destroy(first, last, value_type(first));
+}
+
+// 判断元素数值型别（value type）是否有 trivial destructor
+template <class ForwardIterator, class T>
+inline void __destroy(ForwardIterator first, ForwardIterator last, T*)
+{
+     // 先使用value_type获取迭代器所指对象的型别，
+     // 再使用__type_traits<T>判断该型别的析构函数是否has_trival_destructor
+    typedef typename mystl::__type_traits<T>::has_trival_destructor trivial_destructor;
+    __destroy_aux(first, last, trivial_destructor()); // 编译时确定调用函数
+}
+
+template <class ForwardIterator>
+inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __false_type)
+{
+    for (; first < last; ++first) {
+        destroy(&*first);
+    }
+}
+
+template <class ForwardIterator>
+inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type)
+{}
+
+// destroy 第二版本，针对迭代器为char* 和wchar_t*的特化版
+inline void destroy(char*, char*) {}
+inline void destroy(wchar_t*, wchar_t*) {}
+
+
+}
+
+
+
+#endif
---
+++ stl_type_traits.h
@@ -0,0 +1,150 @@
+#ifndef MYSTL_TYPETRAITS_H_
+#define MYSTL_TYPETRAITS_H_
+
+/**
+ * @brief 实现type_triats
+ * type_traits负责萃取型别特性，此处的型别是指：这个型别是否具备non-trivial default ctor？是否具备non-trivial copy cotr？
+ * 是否具备non-trivial assignment operator？是否具备non-trivial dtor？
+ * 如果是否定的，对这个型别进行构造、析构、拷贝和赋值等操作时，可以采用最直接的措施(例如根本不用调用那些constructor，destructor),
+ * 而采用最直接处理操作，如malloc()、memcpy()等等，获得最高效率。
+ * 
+ */
+
+namespace mystl
+{
+
+struct __true_type {};
+struct __false_type {};
+
+template <class type>
+struct __type_traits {
+    typedef __true_type     this_dummy_member_must_be_first;
+    typedef __false_type    has_trivial_default_constructor;
+    typedef __false_type    has_trivial_copy_constructor;
+    typedef __false_type    has_trivial_assignment_operator;
+    typedef __false_type    has_trivial_destructor;
+    typedef __false_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<char> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<signed char> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<unsigned char> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<short> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<unsigned short> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<int> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<unsigned int> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<long> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<unsigned long> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<float> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<double> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+template <> // 全特化
+struct __type_traits<long double> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+
+
+template <class T> // 偏特化
+struct __type_traits<T*> {
+    typedef __true_type    has_trivial_default_constructor;
+    typedef __true_type    has_trivial_copy_constructor;
+    typedef __true_type    has_trivial_assignment_operator;
+    typedef __true_type    has_trivial_destructor;
+    typedef __true_type    is_POD_type;
+};
+    
+}
+
+
+#endif
